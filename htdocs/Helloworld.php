57e8b47adf8ff848b1acf5fe084363d0e9b50f43
minhtheman <minh.ngo@ucalgary.ca>
2018-04-09 22:21:11 -0600

    Add files via upload
    login and registration

---
+++ htdocs/Helloworld.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>IMPORT CAR DB</h1>
+
+<p><img src="importcardb.png" id="logo" alt="Import Car DB" width="250" height="49" /></p>
+
+<?php
+echo "wuddup";
+?>
+
+</body>
+</html>
